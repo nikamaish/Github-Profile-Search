1511dfd54509f56a7c11b559559df2a70bceea4c
aish <aishnikam31@gmail.com>
2024-02-12 00:33:45 +0530

    home photo done

--- github-repo-search/src/app/app.module.ts
+++ github-repo-search/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './Header/Header.component';  // Import the HeaderComponent
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.page';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
     // Other components
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([])  // Add your routes inside the forRoot
+    RouterModule.forRoot([]),  // Add your routes inside the forRoot
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ github-repo-search/src/app/home/home.page.ts
@@ -0,0 +1,12 @@
+// home.component.ts
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.css']
+})
+export class HomeComponent {
+  // You can add any logic or data needed for the home page
+}
